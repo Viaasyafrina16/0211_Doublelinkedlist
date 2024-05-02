3904107c0d2009f034937cee2e7d6643b82fcb33
Viaasyafrina16 <syafrinavia@gmail.com>
2024-05-03 02:00:15 +0700

    menambhakan switch

--- praktikum9.cpp
+++ praktikum9.cpp
@@ -212,6 +212,30 @@ void searchData()
 }
 
 int main()
+{
+    while (true)
+    {
+        try
+        {
+            cout << "\nMenu" << endl;
+            cout << "1. Add a record from the list" << endl;
+            cout << "2. Delete a record to the list" << endl;
+            cout << "3. View all records in the ascending order of roll numbers" << endl;
+            cout << "4. View all records in the descending order of roll numbers" << endl;
+            cout << "5. Search for a record in the list" << endl;
+            cout << "6. Exit" << endl;
+            char << ch;
+            cin >> ch;
+
+            switch (ch)
+
+            
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        
 
 
 
